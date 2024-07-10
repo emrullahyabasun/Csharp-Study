f8c20a4951bc0ffc23ef76956adc444de11fc036
yabas <yabas@ARCODEUS>
2024-07-10 21:32:10 +0300

    Add project files.

---
+++ TEST/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // the ourAnimals array will store the following: 
+            string animalSpecies = "";
+            string animalID = "";
+            string animalAge = "";
+            string animalPhysicalDescription = "";
+            string animalPersonalityDescription = "";
+            string animalNickname = "";
+
+            // variables that support data entry
+            int maxPets = 8;
+            string? readResult;
+            string menuSelection = "";
+
+            // array used to store runtime data, there is no persisted data
+            string[,] ourAnimals = new string[maxPets, 6];
+
+
+            
+            switch (i)
+            {
+                case 0:
+                    animalSpecies = "dog";
+                    animalID = "d1";
+                    animalAge = "2";
+                    animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+                    animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                    animalNickname = "lola";
+                    break;
+            }
+    }
+}
+
